73dde4e8b544723d8eb2d6bba35c0b2b14f94594
mvttn <85101143+mvttn@users.noreply.github.com>
2024-01-14 13:42:39 +1100

    Add drawing functionality with click and mouseover

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const grid = document.querySelector("#grid");
+/* 
+1. Create grid
+2. Drawing functionality
+3. Change colour (Colour selection option)
+4. Change grid size
+5. Shading function
+*/
 
-window.onload = () => {
+function createGrid() {
+  const grid = document.querySelector("#grid");
   for (let i = 0; i < 16 * 16; i++) {
-    const tile = document.createElement("p");
+    const tile = document.createElement("div");
     tile.classList.add("tile");
+    tile.addEventListener("mouseover", changeTileColour);
+    tile.addEventListener("mousedown", changeTileColour);
     grid.appendChild(tile);
   }
+}
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function changeTileColour(event) {
+  if (event.type === "mouseover" && !mouseDown) {
+    return;
+  }
+  event.target.style.backgroundColor = "black";
+}
+
+window.onload = () => {
+  createGrid();
 };
